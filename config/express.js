47d7a80df7db68d152b4e73f9ecab77ace72764e
oldschool69 <flavio.marcondes@gmail.com>
2020-07-10 08:03:46 -0300

    Refactoring project structure

--- config/express.js
+++ config/express.js
@@ -2,8 +2,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const config = require('config');
 const consign = require('consign');
-const db = require('./database');
-const fetchData = require('../api/data/fechExternalData');
+const db = require('../database/mysql');
+const fetchData = require('../api/external/fech');
 require("dotenv-safe").config();
 
 const app = express()
@@ -15,21 +15,25 @@ app.use(bodyParser.json())
 
 // Endpoints
 consign({ cwd: 'api' })
-    .then('data')
+    .then('external')
     .then('controllers')
     .then('routes')
     .into(app)
 
-// db.createDatabase((error) => {
-//     if (!error){
-//         // Retrieve issues from all projects and populate database
-//         const projects = config.get("projects")
-//         if (projects != null) {
-//             for (i = 0; i < projects.length; i++) {
-//                 fetchData.getOpenedIssues(projects[i]);      
-//             }
-//         }
-//     }
-// })
+// FECH_MODE=init Perform data fetching over initialization
+// FECH_MODE=auto Fetching data will be done automatically by loader module
+if (process.env.FETCH_MODE === "init") {
+    db.createDatabase((error) => {
+        if (!error){
+            // Retrieve issues from all projects and populate database
+            const projects = config.get("projects")
+            if (projects != null) {
+                for (i = 0; i < projects.length; i++) {
+                    fetchData.getOpenedIssues(projects[i]);      
+                }
+            }
+        }
+    })
+}
 
 module.exports.app = app;
