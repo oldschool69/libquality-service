39e29306e85b7ef4dc167bc0a9fe4929daa30815
oldschool69 <flavio.marcondes@gmail.com>
2020-07-10 17:23:22 -0300

    Add scheduler

--- config/express.js
+++ config/express.js
@@ -4,6 +4,7 @@ const config = require('config');
 const consign = require('consign');
 const db = require('../database/mysql');
 const fetchData = require('../api/external/fech');
+const schedule = require('node-schedule');
 require("dotenv-safe").config();
 
 const app = express()
@@ -20,20 +21,32 @@ consign({ cwd: 'api' })
     .then('routes')
     .into(app)
 
-// FECH_MODE=init Perform data fetching over initialization
-// FECH_MODE=auto Fetching data will be done automatically by loader module
-if (process.env.FETCH_MODE === "init") {
+// First time running fetching data from external API
+fetch()
+
+// if FETCH_MODE is auto, leave scheduler
+// featching data every 15 minutes   
+if (process.env.FETCH_MODE == "auto") {
+    const scheduler = schedule.scheduleJob('*/30 * * * *', () => {
+        console.log('***SCHEDULER RUNNING***')
+        fetch()
+    })
+}
+
+function fetch() {
     db.createDatabase((error) => {
-        if (!error){
+        if (!error) {
             // Retrieve issues from all projects and populate database
             const projects = config.get("projects")
             if (projects != null) {
                 for (i = 0; i < projects.length; i++) {
-                    fetchData.getOpenedIssues(projects[i]);      
+                    fetchData.getOpenedIssues(projects[i]);
                 }
             }
         }
     })
 }
+
+
 
 module.exports.app = app;
